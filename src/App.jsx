2b2a52dde5c578becfd9b1de38ac626381941f25
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-27 14:21:42 +0100

    data mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Nav from "./components/Nav.jsx";
 import Card from "./components/Card.jsx";
+import data from "./data.js";
 
 function App() {
+  const cards = data.map(function (item) {
+    return <Card key={item.id} item={item} />;
+  });
   return (
     <div className="container">
       <Nav />
-      <Card />
+      {cards}
     </div>
   );
 }
