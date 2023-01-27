1481afd849f73ef5ea8db3965f9b96de38d47d5f
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-27 13:40:37 +0100

    card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,23 @@
 function Card() {
-  return <div>CARD COMP</div>;
+  return (
+    <div className="card">
+      <img src="./src/assets/japan.jpg" className="card--cover"></img>
+      <div className="card--infos">
+        <img src="./src/assets/marker.png"></img>
+        <span className="card--location">Japan</span>
+        <span className="card--map">
+          <a href="#">View on Google Maps</a>
+        </span>
+        <h1 className="card--title">Mount Fiji</h1>
+        <p className="card--date">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="card--description">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
